f2048dddc96952533a51337408c13573f65e06de
adam-venord <avenord.dev@gmail.com>
2020-09-21 12:09:30 -0400

    added users table and finished auth methods for user model

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
+class User < ApplicationRecord
+
+    validates :username, :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, lenght: { minimum: 6, allow_nil: true }
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(userbname, password) 
+        user = User.find_by(username: username)
+        return user if user && user.is_password?(password)
+        nil
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+
+end
