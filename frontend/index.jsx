2c7eda064ac9e4279f796a8792388c05dcd56fe7
adam-venord <avenord.dev@gmail.com>
2020-11-16 17:55:48 -0500

    Update index.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,12 +3,6 @@ import configureStore from './store/store';
 import Root from './components/root';
 import ReactDOM from 'react-dom';
 
-// TESTING STARTS HERE
-import { signup, login, logout } from './actions/user_actions';
-import { uploadVideo, fetchVideo, fetchVideos } from './actions/video_actions';
-import { fetchComments, createComment, deleteComment } from './actions/comment_actions';
-// TESTING ENDS HERE
-
 document.addEventListener("DOMContentLoaded", () => {
 
     let store;
@@ -26,21 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const root = document.getElementById("root");
-
-    // TESTING STARTS HERE
-    window.getState = store.getState()
-    window.dispatch = store.dispatch
-    window.signup = signup
-    window.login = login
-    window.logout = logout
-    window.fetchVideo = fetchVideo
-    window.fetchVideos = fetchVideos
-    window.uploadVideo = uploadVideo
-
-    window.fetchComments = fetchComments
-    window.createComment = createComment
-    window.deleteComment = deleteComment
-    // TESTING ENDS HERE
-
+    
     ReactDOM.render(<Root store={store} />, root);
 })
